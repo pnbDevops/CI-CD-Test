e0d5d339298097227f2df7b8de1a07e9d0a12f57
pnbDevops <128917872+pnbDevops@users.noreply.github.com>
2025-09-24 11:51:51 +0530

    Update adder.py

--- adder.py
+++ adder.py
@@ -3,7 +3,8 @@ def add_numbers(a, b):
 
 if __name__ == '__main__':
     # This block allows the script to be run directly for demonstration
-    num1 = 5
+    num1 = 15
     num2 = 10
     result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {result}")
+
